dbf0ad6de449feaec90600334a5ac56d5c33c3f4
Nate <nglass1988@gmail.com>
2013-07-16 10:40:06 -0700

    buttton toggle

--- spec/requests/his_spec.rb
+++ spec/requests/his_spec.rb
@@ -5,6 +5,7 @@ describe "tasks" do
     it "display some tasks" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       	visit tasks_path
+      	page.should have_content 'go to bed'
     end
   end
 end
