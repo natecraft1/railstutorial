23e13a7c2974d8a79f6869609c370c6b5e431565
Nate <nglass1988@gmail.com>
2013-07-16 14:31:05 -0700

    some shit

--- Gemfile
+++ Gemfile
@@ -66,4 +66,6 @@ group :test, :development do
 	gem "better_errors"
 	gem "binding_of_caller"
 	gem 'jasmine'
+	gem 'fabrication'
+	gem 'ffaker'
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @password = "123"
+    @email = "joe@chicago.com"
+    
+    @user = User.create(
+      email: @email,
+      password: @password,
+      password_confirmation: @password
+    )
+  end
+  
+  it "encrypts a password" do
+    fish = BCrypt::Engine.hash_secret(@password, @user.salt)
+
+    expect(@user.salt).not_to be_nil
+    expect(@user.fish).to eq(fish)
+  end
+  
+  it "authenticates a user" do
+    authuser = User.authenticate(@email, @password)
+    unauthuser = User.authenticate(@email, "")
+    
+    expect(authuser).to eq(@user)
+    expect(unauthuser).to be_nil
+  end
 end
